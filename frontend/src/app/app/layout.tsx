83937594b9fe7d0910a95d4646e1f1946c710795
Ollie Bednal <obednal97@gmail.com>
2025-05-06 08:26:22 +0100

    update to layout pages

--- frontend/src/app/app/layout.tsx
+++ frontend/src/app/app/layout.tsx
@@ -1,12 +1,6 @@
-'use client';
-
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import LayoutWrapper from "@/components/layout/layoutWrapper";
-import { useUser } from "@/lib/user";
-import type { User } from '@/types/global';
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import ClientLayout from './client-layout';
 import "./globals.css";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
@@ -22,24 +16,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-  const { data: user, loading } = useUser() as { data: User | null; loading: boolean };
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!loading && !user?.token) {
-      router.push("/auth/signIn");
-    }
-  }, [user, loading, router]);
-
-  if (loading || !user?.token) return null;
-
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} bg-background text-foreground antialiased min-h-screen flex flex-col`}
       >
-        <LayoutWrapper>{children}</LayoutWrapper>
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
