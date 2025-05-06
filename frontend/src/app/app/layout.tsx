45543414c3ca84e35968f99f4702f00fcebc4c68
Ollie Bednal <obednal97@gmail.com>
2025-05-06 08:28:43 +0100

    update link to global.css

--- frontend/src/app/app/layout.tsx
+++ frontend/src/app/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import ClientLayout from './client-layout';
-import "./globals.css";
+import "../globals.css";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
