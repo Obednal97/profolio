d8801664e500d77e61da917fcb54be18adb99d40
Ollie Bednal <obednal97@gmail.com>
2025-05-31 09:03:44 +0100

    🧹 Remove backend/dist from Git tracking - build artifacts should not be versioned

--- backend/dist/lib/validate.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateRequiredFields = validateRequiredFields;
-function validateRequiredFields(obj, fields) {
-    return fields.filter((field) => {
-        const value = obj[field];
-        return value === undefined || value === null || value === '';
-    });
-}
